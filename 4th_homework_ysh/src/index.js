b3a2d78b44ed4192cd3bf9883c09f2479f49aebe
shyoon840 <shyoon840@handong.ac.kr>
2023-05-11 00:34:16 +0900

    create firebase.js

--- 4th_homework_ysh/src/index.js
+++ 4th_homework_ysh/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import firebaseapp from './firebase';
+console.log(firebaseapp);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
